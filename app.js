57a0ccbcb69aed3f646f7b947371a7ffa017ee69
Jenny <ricejl@gmail.com>
2019-11-16 16:01:40 -0700

    create buttons and associated functions

--- app.js
+++ app.js
@@ -1,58 +1,108 @@
-let cheese = 0;
-let cheeseCountElem = document.querySelector("#cheese-count");
-let pickaxeElem = document.querySelector("#pickaxe");
+let plasticity = 0;
+let plasticityAmtElem = document.querySelector("#plasticity-amt");
+let showerElem = document.querySelector("#shower");
+let quitFbElem = document.querySelector("#quit-fb");
+let summonYogiElem = document.querySelector("#yogi");
+let submersionCbdElem = document.querySelector("#cbd-oil");
 
 let clickUpgrades = {
-  pickaxes: {
+  showerThink: {
     price: 10,
     quantity: 0,
     multiplier: 1
   },
-  cart: {
-    price: 25,
+  quitFb: {
+    price: 15,
     quantity: 0,
     multiplier: 5
   }
 };
 
 let automaticUpgrades = {
-  rovers: {
-    price: 600,
+  yogi: {
+    price: 20,
     quantity: 0,
     multiplier: 20
+  },
+  cbdOil: {
+    price: 25,
+    quantity: 0,
+    multiplier: 10
   }
 };
 
 //for click multiplier: use for (variable in object) to do this
 
-function mine() {
-  cheese++;
-  clickUpgrades.pickaxes.quantity;
-  cheese += clickUpgrades.pickaxes.multiplier * clickUpgrades.pickaxes.quantity;
+function sleep() {
+  plasticity++;
+  clickUpgrades.showerThink.quantity;
+  plasticity +=
+    clickUpgrades.showerThink.multiplier * clickUpgrades.showerThink.quantity;
+  plasticity += clickUpgrades.quitFb.multiplier * clickUpgrades.quitFb.quantity;
   update();
+}
+
+function update() {
+  plasticityAmtElem.innerHTML = plasticity;
+  showerElem.innerHTML = clickUpgrades.showerThink.quantity;
+  quitFbElem.innerHTML = clickUpgrades.quitFb.quantity;
+  summonYogiElem.innerHTML = automaticUpgrades.yogi.quantity;
+  submersionCbdElem.innerHTML = automaticUpgrades.cbdOil.quantity;
 }
 
 //use for in loop to iterate over upgrades
 
-function update() {
-  cheeseCountElem.innerHTML = cheese;
-  pickaxeElem.innerHTML = clickUpgrades.pickaxes.quantity;
+// function collectClickUpgrades() {}
+
+function showerThink() {
+  if (plasticity >= clickUpgrades.showerThink.price) {
+    clickUpgrades.showerThink.quantity++;
+    plasticity -= clickUpgrades.showerThink.price;
+    console.log("shower think: " + plasticity);
+  }
+  update();
 }
 
-function BuyPickaxe() {
-  //checks if the user has the resources to purchase and if so increase the pickaxe count and decrease cheese resources by appropriate amt; use console.log to test it's working--once working add count to page to user knows how many they have
-  if (cheese >= clickUpgrades.pickaxes.price) {
-    clickUpgrades.pickaxes.quantity++;
-    cheese -= clickUpgrades.pickaxes.price;
-    console.log(clickUpgrades.pickaxes.quantity);
-    console.log(cheese);
+function quitFb() {
+  if (plasticity >= clickUpgrades.quitFb.price) {
+    clickUpgrades.quitFb.quantity++;
+    plasticity -= clickUpgrades.quitFb.price;
+    console.log("quit fb: " + plasticity);
+  }
+  update();
+}
+
+function summonYogi() {
+  if (plasticity >= automaticUpgrades.yogi.price) {
+    automaticUpgrades.yogi.quantity++;
+    plasticity -= automaticUpgrades.yogi.price;
+    console.log("yogi: " + plasticity);
+  }
+  update();
+}
+
+function submersionCBD() {
+  if (plasticity >= automaticUpgrades.cbdOil.price) {
+    automaticUpgrades.cbdOil.quantity++;
+    plasticity -= automaticUpgrades.cbdOil.price;
+    console.log("CBD: " + plasticity);
   }
   update();
 }
 
 function collectAutoUpgrades() {
-  //iterate over autoUpgrades, total quantity of each autoUpgrade times multiplier, then add that value to cheese resource
+  //iterate over autoUpgrades, total quantity of each autoUpgrade times multiplier, then add that value to plasticity resource
   //setInterval to make sure this occurs every 3 seconds automatically and don't forget to invoke it
+  for (let quantity in automaticUpgrades) {
+    if (automaticUpgrades.hasOwnProperty(quantity, multiplier)) {
+      plasticity += automaticUpgrades[quantity] * automaticUpgrades[multiplier];
+    }
+  }
 }
 
+function startInterval() {
+  collectionInterval = setInterval(collectAutoUpgrades, 3000);
+}
+
+// startInterval(); when either quantity of summon yogi or cbd oil is > 0
 update();
